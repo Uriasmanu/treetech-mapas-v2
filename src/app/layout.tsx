09614b8ce0ae0d531bccccf4970f16f65917fe54
Manoela Urias <maanoelaurias@gmail.com>
2024-12-09 14:28:37 -0300

    estlização do modelo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import { AppSidebar } from "@/components/app-sidebar";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -24,11 +26,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+
     <html lang="pt-br">
+
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <SidebarProvider>
+          <main className="flex p-4 ">
+            <AppSidebar />
+            <SidebarTrigger />
+            {children}
+          </main>
+        </SidebarProvider>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
 export default function HomePage() {
   return (
     <div>
-      <h1>Bem-vinda à página principal!</h1>
-      
+
     </div>
   );
 }
