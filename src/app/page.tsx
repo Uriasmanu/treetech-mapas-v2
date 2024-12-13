674644749e4ef94b4c7b4300aa714df36356add6
Manoela Urias <maanoelaurias@gmail.com>
2024-12-12 21:35:03 -0300

    Atualização dos estilos e adição de novos componentes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,45 +1,46 @@
 import Image from 'next/image';
 import logo from './logo.png';
+import planilhaIcon from '../image/planilha.png';
+import planilhaIcon2 from '../image/planilha2.png';
+import BotaoAtualizar from '@/component/BotaoAtualizar/BotaoAtualizar';
 
 export default function Home() {
-  
+
   return (
     <div className='flex-col flex items-center w-[100vw] h-[100vh] py-8'>
-      <header className='w-full flex-col items-centerr'>
+      <header className='w-full flex flex-col items-center gap-3 py-4'>
         <div className='flex items-center justify-center gap-4'>
-          <h1 className='text-3xl'>Treetech Mapas</h1>
-          <Image src={logo} alt="Logo da Empresa" className='w-[80px] h-[80px]'/>
+          <h1 className='text-6xl font-bold'>Treetech Mapas</h1>
+          <Image src={logo} alt="Logo da Empresa" className='w-[80px] h-[80px]' />
         </div>
-        <p className=' text-center'>Sistema que automatiza o processo de atualização dos Mnemônicos de acordo com a verção anterior do mapeamento.</p>
       </header>
 
-      <main className='w-full h-full bg-green-500'>
-        <section>
-          <h2>Arquivo para Comparação</h2>
-          <p>Arraste e solte o arquivo ou clique para escolher o arquivo CSV que precisa ser adicionado. A coluna e os mnemonicos serão usados para comparação.</p>
-          <div id="file-upload-comparison">
+      <main className="w-full h-screen bg-green-500 p-4 flex flex-col items-center justify-center gap-8">
+        <section className='flex gap-16 justify-center'>
 
+          <div className="p-28 border-4 border-dashed border-green-900">
+            <input type="file" className="hidden" id="file-upload" />
+            <label htmlFor="file-upload" className="flex flex-col items-center cursor-pointer w-[20rem] text-center mx-auto gap-4">
+              <Image src={planilhaIcon} alt="Ícone de planilha" className='w-[30px] h-[30px]' />
+              <h2 className="text-base font-bold ">Arraste aqui ou selecione o mapeamento que precisa ser atualizado</h2>
+            </label>
           </div>
-        </section>
 
-        <section>
-          <h2>Arquivo para Descrição</h2>
-          <p>Arraste e solte o arquivo ou clique para escolher o arquivo CSV que será usado para fornecer a descrição dos mnemonicos de acordo com os IDs.</p>
-          <div id="file-upload-description">
-
+          <div className="p-28 border-4 border-dashed border-green-900">
+            <input type="file" className="hidden" id="file-upload" />
+            <label htmlFor="file-upload" className="flex flex-col items-center cursor-pointer w-[20rem] text-center mx-auto gap-4">
+              <Image src={planilhaIcon2} alt="Ícone de planilha" className='w-[30px] h-[30px]' />
+              <h2 className="text-base font-bold ">Arraste aqui ou selecione o mapeamento que contem os Mnemônicos</h2>
+            </label>
           </div>
+
         </section>
 
-        <section>
-          <button id="start-process">Iniciar Processo</button>
+        <section className='flex gap-10 justify-center'>
+          <BotaoAtualizar/>
+
         </section>
 
-        <section>
-          <h2>Progresso</h2>
-          <div id="progress-bar">
-
-          </div>
-        </section>
       </main>
 
     </div>
---
+++ src/component/BotaoAtualizar/BotaoAtualizar.tsx
@@ -0,0 +1,9 @@
+import './BotaoAtualizar.css';
+
+export default function BotaoAtualizar() {
+    return (
+        <button className="botao-atualizar">
+            Atualizar
+        </button>
+    );
+}
