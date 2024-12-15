80a1e667839092659a84c9618427c4e98ca1d1b6
Manoela Urias <maanoelaurias@gmail.com>
2024-12-15 16:48:08 -0300

    adicionando validação

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,6 +15,10 @@ export default function Home() {
   const [loading, setLoading] = useState(false);
 
   const handleClick = () => {
+    if (!novaPlanilha || !planilhaCompleta) {
+      return;
+    }
+
     setLoading(true);
     console.log('Nova Planilha:', novaPlanilha);
     console.log('Planilha Completa:', planilhaCompleta);
