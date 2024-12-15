63dcf1c400da0a2faaf27e2d6e1ef0fd3a493643
Manoela Urias <maanoelaurias@gmail.com>
2024-12-15 18:31:42 -0300

    criação do componente file

---
+++ src/component/ComponenteFile/ComponenteFile.tsx
@@ -0,0 +1,8 @@
+
+export default function ComponenteFile() {
+    <div className="card">
+        <div className="card_load"></div>
+        <div className="card_load_extreme_title"></div>
+        <div className="card_load_extreme_descripion"></div>
+    </div>
+}
